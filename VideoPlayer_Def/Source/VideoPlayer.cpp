9cd8ac6d9b949ad869b0e7d374d355907a224771
DaniMariagesUPC <99950347+DaniMariages@users.noreply.github.com>
2023-05-02 23:23:42 +0200

    Fix

--- VideoPlayer_Def/Source/VideoPlayer.cpp
+++ VideoPlayer_Def/Source/VideoPlayer.cpp
@@ -13,22 +13,22 @@ using namespace std;
 
 VideoPlayer::VideoPlayer()
 {
-    cv::VideoCapture video("Clip.mp4");
+    VideoCapture video("Clip.mp4");
 
-    cv::Mat frame;
+    Mat frame;
 
-    int frameWidth = video.get(cv::CAP_PROP_FRAME_WIDTH);
-    int frameHeight = video.get(cv::CAP_PROP_FRAME_HEIGHT);
+    int frameWidth = video.get(CAP_PROP_FRAME_WIDTH);
+    int frameHeight = video.get(CAP_PROP_FRAME_HEIGHT);
 
-    cv::VideoWriter output("output.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 60, cv::Size(100, 100));
+    VideoWriter output("output.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), 60, Size(100, 100));
 
     while (video.read(frame))
     {
-        cv::imshow("Video feed", frame);
+        imshow("Video feed", frame);
 
         output.write(frame);
 
-        if (cv::waitKey(25) >= 0)
+        if (waitKey(25) >= 0)
         {
             break;
         }
@@ -38,5 +38,5 @@ VideoPlayer::VideoPlayer()
     output.release();
     video.release();
 
-    cv::destroyAllWindows();
+    destroyAllWindows();
 }
