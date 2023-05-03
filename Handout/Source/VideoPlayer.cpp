afeb3a7c2160b4d14c362f1dd7f99b17e84d0dc5
DaniMariagesUPC <99950347+DaniMariages@users.noreply.github.com>
2023-05-03 10:34:45 +0200

    .

--- Handout/Source/VideoPlayer.cpp
+++ Handout/Source/VideoPlayer.cpp
@@ -15,24 +15,24 @@ VideoPlayer::VideoPlayer()
 {
     //TODO 1: Open the video file you want to reproduce
 
-    //TODO 2:Save the video frames using de class Mat (Matrix)
+    //TODO 2:Save the video frames using the class Mat (Matrix)
 
 
-    //TODO 6: Get width and height from the video we want to reproduce usin video.get()
+    //TODO 6: Get width and height from the video we want to reproduce using video.get()
 
 
 
 
-    //TODO 7: Create a video righter from the class VideoWriter
+    //TODO 7: Create a video writer from the class VideoWriter
     
 
 
-    //TODO 3: Create a loop for reading the video, when the video ends or a "esc" is pressed it will close
+    //TODO 3: Create a loop for reading the video, when the video ends or "esc" is pressed it will close
     while ()
     {
         //TODO 4: Use the imshow() function to give a name for the window and choose what you want to display, in this case the video frame
 
-        //TODO 8: Write / Show every frame using de videowriter we created before
+        //TODO 8: Write / Show every frame using the videowriter we created before
         
 
         //TODO 5: Create a loop(if) using WaitKey() for stopping the video
